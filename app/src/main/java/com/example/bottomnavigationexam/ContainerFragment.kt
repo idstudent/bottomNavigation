02fdfb6c38e9931390f75c724ee4f00f106c0724
sm1374 <36175704+idstudent@users.noreply.github.com>
2021-10-09 18:23:23 +0900

    바텀네비게이션뷰 예제

---
+++ app/src/main/java/com/example/bottomnavigationexam/ContainerFragment.kt
@@ -0,0 +1,17 @@
+package com.example.bottomnavigationexam
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ContainerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_container, container, false)
+    }
+}
